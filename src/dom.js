ab183a2bf528991fe5ca74a8caa4231269757e88
Hui Wang <hui.wang@bizfocus.cn>
2021-07-19 13:59:36 +0800

    update src/dom.js & version

--- src/dom.js
+++ src/dom.js
@@ -10,12 +10,12 @@ export const createDOMProject = async (
     branch
 ) => {
     try {
-        loading.text = '创建项目中...';
+        loading.text = 'creating project...';
         loading.color = 'green';
         loading.start();
         await downloadRepo(`HuiWang111/react-ts-templete#bizfocus/${branch}`, projectPath);
         loading.stop();
-        console.info('项目创建成功！');
+        console.info('created !!!');
     
         // 更新模板的package.json的name字符
         updateProjectPackageJson(projectPath);
@@ -23,7 +23,7 @@ export const createDOMProject = async (
         // 删除README.md
         fs.unlinkSync(projectPath + '/README.md');
     
-        console.info('开始安装依赖')
+        console.info('install dependencies');
         // 安装依赖 & 生成dll文件
         await execa('npm', ['ci'], {
             cwd: projectPath,
@@ -34,7 +34,7 @@ export const createDOMProject = async (
         //     cwd: projectPath,
         //     stdio: [2, 2, 2]
         // });
-        console.info('依赖安装完成！');
+        console.info('installed !!!');
         console.info('')
         console.info('------------------------------------------------------')
         console.info(`运行 cd ${project} && npm run dll && npm start 启动项目`)
