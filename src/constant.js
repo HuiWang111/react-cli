c0a8597e5b8a7b351c447b10f511de07db901918
Hui Wang <hui.wang@bizfocus.cn>
2021-08-05 17:36:20 +0800

    增加 react-native 项目scripts复制 & react-native版本检测

--- src/constant.js
+++ src/constant.js
@@ -13,3 +13,5 @@ export const Envs = ['React-DOM', 'React-Native'];
 export const NativeSourcePath = './RNTemplete';
 
 export const NativeSourceSrcPath = `${NativeSourcePath}/src`;
+
+export const NativeSourceScriptsPath = `${NativeSourcePath}/scripts`;
--- src/native.js
+++ src/native.js
@@ -1,7 +1,7 @@
 import execa from 'execa';
 import fs from 'fs';
 import path from 'path';
-import { NativeSourcePath, NativeSourceSrcPath } from './constant.js';
+import { NativeSourcePath, NativeSourceSrcPath, NativeSourceScriptsPath } from './constant.js';
 import { rimrafSync } from './rimrafSync.js';
 import { downloadRepo } from './download.js';
 
@@ -61,13 +61,18 @@ export const createNativeProject = async (
         writeFile('babel.config.js', fs.readFileSync(path.join(process.cwd(), NativeSourcePath, 'babel.config.js')));
         writeFile('tsconfig.json', fs.readFileSync(path.join(process.cwd(), NativeSourcePath, 'tsconfig.json')));
         writeFile('index.js', fs.readFileSync(path.join(process.cwd(), NativeSourcePath, 'index.js')));
+        updatePackageJSON(path.join(projectPath, 'package.json'));
 
         const src = path.join(process.cwd(), NativeSourceSrcPath);
-        const dir = path.join(projectPath, 'src');
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
-        copy(fs.readdirSync(src));
+        const scripts = path.join(process.cwd(), NativeSourceScriptsPath);
+        ['src', 'scripts'].forEach(dirname => {
+            const dir = path.join(projectPath, dirname);
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            }
+        });
+        copy(fs.readdirSync(scripts), scripts, 'scripts');
+        copy(fs.readdirSync(src), src, 'src');
 
         loading.stop();
         console.info('project initialized');
@@ -88,29 +93,43 @@ export const createNativeProject = async (
         console.info(`writeFile: ${filePath}`);
     }
 
-    function copy(paths, currentPath = path.join(process.cwd(), NativeSourceSrcPath)) {
+    function copy(paths, currentPath, separator) {
         const dirPath = currentPath;
 
         paths.forEach(p => {
             currentPath = path.join(dirPath, p);
             const stat = fs.statSync(currentPath);
-            const pathTail = currentPath.split('src')[1].slice(1);
+            const pathTail = currentPath.split(separator)[1].slice(1);
 
             if (stat.isDirectory()) {
                 if (pathTail) {
-                    const dir = path.join(projectPath, 'src', pathTail);
+                    const dir = path.join(projectPath, separator, pathTail);
 
                     if (!fs.existsSync(dir)) {
                         fs.mkdirSync(dir);
                     }
                 }
-                copy(fs.readdirSync(currentPath), currentPath);
+                copy(fs.readdirSync(currentPath), currentPath, separator);
             } else {
                 writeFile(
-                    path.join('src', pathTail),
+                    path.join(separator, pathTail),
                     fs.readFileSync(currentPath)
                 );
             }
         });
+    }
+
+    function updatePackageJSON(pkgFile) {
+        try {
+            const pkg = JSON.parse(fs.readFileSync(pkgFile).toString());
+            pkg.scripts.start = "npm run check-version && react-native start";
+            pkg.scripts['check-version'] = "node --experimental-json-modules scripts/checkDepsVersion.mjs";
+            fs.writeFileSync(
+                pkgFile,
+                JSON.stringify(pkg, "", "\t")
+            );
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
